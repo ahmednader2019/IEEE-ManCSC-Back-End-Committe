30ceddfc7443b4a656ad35db0acb54e166b97e9a
Ahmed Nader <57724930+ahmednader2019@users.noreply.github.com>
2022-10-18 17:32:00 +0200

    Add files via upload

---
+++ php_tasks/Task-3/Task3.php
@@ -0,0 +1,24 @@
+<?php
+
+if($_POST['grade']>=85)
+echo "Excellant";
+else if($_POST['grade']>=75)
+echo "Very good";
+else if($_POST['grade']>=65)
+echo "good";
+else if($_POST['grade']>=50)
+echo "Passed";
+else
+echo "Failed";
+
+
+
+
+
+
+
+
+
+
+
+
